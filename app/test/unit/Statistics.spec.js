7fa0d89fd6efa5a430b154b7b0623c55cbdd9111
Christian Fei <crifei93@gmail.com>
2015-05-25 08:10:51 +0200

    added first failing statistics test

---
+++ app/test/unit/Statistics.spec.js
@@ -0,0 +1,14 @@
+describe('Statistics', function() {
+  var Statistics
+
+  beforeEach(inject(function(_Statistics_){
+    Statistics = _Statistics_
+  }))
+
+
+  it('fails', function() {
+    expect(0).toBeFalsy()
+  })
+
+
+})
