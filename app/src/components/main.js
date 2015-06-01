bace82595e28246b464027989ece1bb2325be993
Christian Fei <crifei93@gmail.com>
2015-06-01 12:03:40 +0200

    added templtae pomodoro

--- app/src/components/main.js
+++ app/src/components/main.js
@@ -1,8 +1,10 @@
 var React = require('react')
+var Pomodoro = require('./pomodoro')
 
 module.exports = React.createClass({
   render: function() {
     return  <div className="main">
+              <Pomodoro/>
             </div>
   }
 })
---
+++ app/src/components/pomodoro.js
@@ -0,0 +1,9 @@
+var React = require('react')
+
+module.exports = React.createClass({
+  render: function(){
+    return  <div className="pomodoro">
+              <h1>00:00</h1>
+            </div>
+  }
+})
