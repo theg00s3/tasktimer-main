c2d2ed46be1c995d2cf6102264399e6b05659f45
Christian Fei <crifei93@gmail.com>
2015-06-01 23:00:44 +0200

    styled about page

--- app/src/pages/about.js
+++ app/src/pages/about.js
@@ -3,7 +3,17 @@ var React = require('react')
 module.exports = function(context){
   React.render(
     <div className="content">
-      <h1>About</h1>
+      <h1 className="tac">Boost your productivity</h1>
+      <h5 className="tac">Manage your time more effectively</h5>
+
+      <p className="breath">
+        This tool will keep help you to keep track of your work and review your achieved goals for the day.
+        <br/>
+        By following the guidelines of the
+        <a className="simple" href="http://pomodorotechnique.com/" target="_blank">Pomodoro technique &reg;</a>
+        you ll be able to boost your productivity, work in more concentrated bursts and have frequent, mind refreshing breaks.
+      </p>
+      <a className="button block tac first-pomodoro-cta" href="/">start your first pomodoro!</a>
     </div>,
   document.querySelector('main'))
 }
