511c07de4117920792dcb8c1ab1b241876c8b134
Christian Fei <crifei93@gmail.com>
2015-06-01 18:24:34 +0200

    wip restore pomdoroo

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -3,7 +3,15 @@ var Pomodoro = require('./Pomodoro')
 
 module.exports = React.createClass({
   render: function() {
-    var remaining = parseInt(localStorage.remaining, 10)
+    var pomodoroData = {}
+    var remaining = 0
+    try {
+      pomodoroData = JSON.parse(localStorage.pomodoroData)
+    }catch(e){}
+    if( pomodoroData && pomodoroData.minutes && pomodoroData.startedAt ){
+      remaining = parseInt((moment(pomodoroData.startedAt).unix()+pomodoroData.minutes*60 - moment().unix()),10)
+      debugger
+    }
     return  <div className="main">
               <Pomodoro remaining={remaining}/>
             </div>
