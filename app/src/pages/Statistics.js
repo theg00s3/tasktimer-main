0323cd401d232cbccf8712f481c7dadefbf25860
Christian Fei <crifei93@gmail.com>
2015-06-02 20:56:57 +0200

    added statistics page with debug data

--- app/src/pages/Statistics.js
+++ app/src/pages/Statistics.js
@@ -1,14 +1,50 @@
+var Loader = require('react-loader')
 var React = require('react')
+var axios = require('axios')
 
 module.exports = function(context){
-  React.render(<Statistics></Statistics>, document.querySelector('main'))
+  var day = '02/06/2015'
+  var dataPromise = axios.get('/api/pomodoro',{
+    params: {
+      day: day
+    }
+  })
+  React.render(<Statistics dataPromise={dataPromise}></Statistics>, document.querySelector('main'))
 }
 
 
 var Statistics = React.createClass({
+  getInitialState: function(){
+    return {
+      data: null,
+      loaded: false
+    }
+  },
+  componentDidMount: function(){
+    this.props.dataPromise
+      .then(function(response){
+        this.setState({
+          data: response.data,
+          loaded: true
+        })
+      }.bind(this))
+  },
   render: function(){
-    return  <div className="content">
-              <h1 className="tac">Statistics</h1>
-            </div>
+    var data = this.state.data || []
+
+    var debugData = data.map(function(dat){
+      return  <li>
+                {dat.minutes} - {dat.type} - {dat.startedAt}
+              </li>
+    })
+
+    return  <Loader loaded={this.state.loaded}>
+              <div className="content">
+                <h1 className="tac">Statistics</h1>
+                <ul>
+                  {debugData}
+                </ul>
+              </div>
+            </Loader>
   }
 })
