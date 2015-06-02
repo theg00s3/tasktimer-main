cf322c9c0db3c56fd4292248a1b1257b5ce29bf5
Christian Fei <crifei93@gmail.com>
2015-06-02 16:47:07 +0200

    added links for gridmenu

--- app/src/components/GridMenu.js
+++ app/src/components/GridMenu.js
@@ -3,21 +3,21 @@ var React = require('react')
 module.exports = React.createClass({
   render: function(){
     return  <div className={"row grid-menu " + this.props.className}>
-              <div className="col">
+              <a href="/statistics" className="col">
                 <h5 className="heading">Statistics</h5>
                 <i className="icon main-icon ion-stats-bars"></i>
                 <span className="lead">Keep track of your work and review it with insightful statistics.</span>
-              </div>
-              <div className="col">
+              </a>
+              <a href="/public" className="col">
                 <h5 className="heading">Public rooms</h5>
                 <i className="icon main-icon ion-ios-people"></i>
                 <span className="lead">Meetings with remote teams or remote pair pair programming sessions stay in sync with a shared pomodoro</span>
-              </div>
-              <div className="col">
+              </a>
+              <a href="/about" className="col">
                 <h5 className="heading">About</h5>
                 <i className="icon main-icon ion-ios-help-outline"></i>
                 <span className="lead">Pomodoro.cc helps you to stay concentrated.</span>
-              </div>
+              </a>
             </div>
   }
 })
