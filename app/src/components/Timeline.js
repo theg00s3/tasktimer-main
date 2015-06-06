a98ef9091ba84cea7df173051985717a289772ab
Christian Fei <crifei93@gmail.com>
2015-06-06 15:35:51 +0200

    formatting in Timeline component

--- app/src/components/Timeline.js
+++ app/src/components/Timeline.js
@@ -1,10 +1,15 @@
 var React = require('react')
+var moment = require('moment')
+var PomodoroUtils = require('../../../shared/PomodoroUtils')
 
 module.exports = React.createClass({
   render: function(){
     var timeline = this.props.data.map(function(dataPoint){
+      var formattedStartedAt = moment(dataPoint.startedAt).format('HH:mm')
+      var duration = PomodoroUtils.getDuration(dataPoint)
       return  <li className={"type-"+dataPoint.type}>
-                {dataPoint.minutes} - {dataPoint.startedAt}
+                <span className="duration">{duration}</span>
+                <span className="startedAt">{formattedStartedAt}</span>
               </li>
     })
 
