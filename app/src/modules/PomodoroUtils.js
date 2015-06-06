54ce00401a56be9f4fe4259d2f6767779eb152b5
Christian Fei <crifei93@gmail.com>
2015-06-06 13:23:21 +0200

    added test for getDuration for cancelled pomodoor

--- app/src/modules/PomodoroUtils.js
+++ app/src/modules/PomodoroUtils.js
@@ -6,8 +6,11 @@ function getDuration(pomodoro){
   if( !pomodoro || pomodoro.startedAt === undefined || pomodoro.minutes === undefined ) {
     return 0
   }
-  if( pomodoro.cancelledAt === undefined && pomodoro.minutes ){
+  if( !pomodoro.cancelledAt ){
     return pomodoro.minutes * 60
+  }
+  if( pomodoro.cancelledAt ) {
+    return parseInt((pomodoro.cancelledAt - pomodoro.startedAt)/1000, 10)
   }
   return 0
 }
--- app/test/unit/PomodoroUtils.js
+++ app/test/unit/PomodoroUtils.js
@@ -1,12 +1,21 @@
 var expect = require('chai').expect
 
 describe('PomodoroUtils', function () {
+  var PomodoroUtils = require('../../src/modules/PomodoroUtils')
+
   it('calculates the duration of a pomodoro', function () {
     var pomodoro = {
       minutes: 25,
       startedAt: Date.now()
     }
-    var PomodoroUtils = require('../../src/modules/PomodoroUtils')
     expect( PomodoroUtils.getDuration(pomodoro) ).to.equal(25*60)
+  })
+  it('calculates the duration of a cancelled pomodoro', function () {
+    var pomodoro = {
+      minutes: 25,
+      startedAt: Date.now() - 20*60*1000,
+      cancelledAt: Date.now()
+    }
+    expect( PomodoroUtils.getDuration(pomodoro) ).to.equal(20*60)
   })
 })
