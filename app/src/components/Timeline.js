32be7439bbdc74a2434d77ee266ebce687287d3f
Christian Fei <crifei93@gmail.com>
2015-06-06 16:23:45 +0200

    added duration to timeline

--- app/src/components/Timeline.js
+++ app/src/components/Timeline.js
@@ -6,9 +6,9 @@ module.exports = React.createClass({
   render: function(){
     var timeline = this.props.data.map(function(dataPoint){
       var formattedStartedAt = moment(dataPoint.startedAt).format('HH:mm')
-      var duration = PomodoroUtils.getDuration(dataPoint)
+      var duration = PomodoroUtils.getDurationInMinutes(dataPoint)
       return  <li className={"type-"+dataPoint.type}>
-                <span className="duration">{duration}</span>
+                <span className="duration">Duration: {duration}min</span>
                 <span className="startedAt">{formattedStartedAt}</span>
               </li>
     })
