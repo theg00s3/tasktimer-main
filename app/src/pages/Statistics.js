31263f84f35e0885204f67320cf84a83a9bd5694
Christian Fei <crifei93@gmail.com>
2015-06-06 12:23:18 +0200

    basic integration of chart.js

--- app/src/pages/Statistics.js
+++ app/src/pages/Statistics.js
@@ -2,6 +2,9 @@ var Loader = require('react-loader')
 var React = require('react')
 var axios = require('axios')
 
+var PieChart = require("react-chartjs").Pie
+
+
 module.exports = function(context){
   var day = '02/06/2015'
   var dataPromise = axios.get('/api/pomodoro',{
@@ -12,6 +15,36 @@ module.exports = function(context){
   React.render(<Statistics dataPromise={dataPromise}></Statistics>, document.querySelector('main'))
 }
 
+var chartData = [{
+  value: 300,
+  color:"#DF2E2E",
+  highlight: "#DF2E2E",
+  label: "Pomodori"
+},
+{
+  value: 50,
+  color: "#24b524",
+  highlight: "#24b524",
+  label: "Breaks"
+}]
+
+var chartOptions = {
+  //Number - The percentage of the chart that we cut out of the middle
+  percentageInnerCutout : 35, // This is 0 for Pie charts
+
+  //Number - Amount of animation steps
+  animationSteps : 100,
+
+  //String - Animation easing effect
+  animationEasing : "easeOutBounce",
+
+  //Boolean - Whether we animate the rotation of the Doughnut
+  animateRotate : true,
+
+  //Boolean - Whether we animate scaling the Doughnut from the centre
+  animateScale : false,
+
+}
 
 var Statistics = React.createClass({
   getInitialState: function(){
@@ -46,6 +79,7 @@ var Statistics = React.createClass({
                 <ul>
                   {debugData}
                 </ul>
+                <PieChart data={chartData} options={chartOptions}/>
               </div>
             </Loader>
   }
