785236e10bc04149d18c8cb3c10defef7a09bf9c
Christian Fei <crifei93@gmail.com>
2015-06-07 16:17:47 +0200

    tracking timer-start and timer-end events

--- app/src/modules/PomodoroEventHandler.js
+++ app/src/modules/PomodoroEventHandler.js
@@ -1,19 +1,23 @@
 var store = require('store')
 var PomodoroService = require('./PomodoroService')
 var constants = require('../constants')
+var AnalyticsService = require('./AnalyticsService')
 
 module.exports = function(eventName, minutes, type, time){
   switch( eventName ){
     case 'start':
-      store.set('pomodoroData', {
+      var pomodoroData = {
         minutes: minutes,
         type: type,
         startedAt: Date.now()
-      })
+      }
+      AnalyticsService.track('timer-start', pomodoroData)
+      store.set('pomodoroData', pomodoroData)
       break
     case 'end':
       var pomodoroData = store.get('pomodoroData')
       pomodoroData.cancelledAt = Date.now()
+      AnalyticsService.track('timer-end', pomodoroData)
       PomodoroService.create(pomodoroData)
       store.remove('pomodoroData')
       resetTitle()
