db5726c02bba602e4bfd9328b4673084a67634f0
Christian Fei <crifei93@gmail.com>
2015-06-08 19:53:44 +0200

    refactored ChartUtils tests

--- app/test/unit/ChartUtils.js
+++ app/test/unit/ChartUtils.js
@@ -4,15 +4,22 @@ var expect = require('chai').expect
 
 describe('ChartUtils', function () {
   it('gets chart data for pie chart', function () {
+    var durationFirstPomodoro = 25
+    var durationSecondPomodoro = 5
     var data = [{
       type: 'pomodoro',
       minutes: 25,
       startedAt: Date.now()
+    }, {
+      type: 'pomodoro',
+      minutes: 25,
+      startedAt: Date.now(),
+      cancelledAt: Date.now() + durationSecondPomodoro * 60 * 1000
     }]
     var chartData = ChartUtils.getPieChartDataFrom(data)
     expect( chartData[0] ).to.be.defined
     expect( chartData[1] ).to.be.defined
-    expect( chartData[0].value ).to.equal(25)
+    expect( chartData[0].value ).to.equal( durationFirstPomodoro + durationSecondPomodoro )
     expect( chartData[1].value ).to.equal(0)
   })
 })
