85c793a9f4db52436c288710071f239861e68752
Christian Fei <crifei93@gmail.com>
2015-06-08 19:39:55 +0200

    added failing CHartUtils test

---
+++ app/test/unit/ChartUtils.js
@@ -0,0 +1,9 @@
+var ChartUtils = require('../../src/modules/ChartUtils')
+
+var expect = require('chai').expect
+
+describe('ChartUtils', function () {
+  it('fails', function () {
+    expect(42).to.be.false
+  })
+})
