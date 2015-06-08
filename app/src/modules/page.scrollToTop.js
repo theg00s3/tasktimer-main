15a9e9523fe7b1a3eb1566e8b99be7e028b4347a
Christian Fei <crifei93@gmail.com>
2015-06-08 08:21:54 +0200

    added exception for statistics page for scrollToTop module

--- app/src/modules/page.scrollToTop.js
+++ app/src/modules/page.scrollToTop.js
@@ -1,8 +1,17 @@
-module.exports = function(context, next){
-  if( window.scrollTo )
-    setTimeout(function(){
-      window.scrollTo(0,0)
+var _ = require('underscore')
+module.exports = function(exceptions){
+  exceptions = exceptions || []
+  return function(context, next){
+    var isExcpection = _.some(exceptions, function(value, i, list){
+      var regexp = new RegExp(value)
+      return regexp.test(context.pathname)
     })
+    if( window.scrollTo && !isExcpection){
+      setTimeout(function(){
+        window.scrollTo(0,0)
+      })
+    }
 
-  next()
+    next()
+  }
 }
--- app/src/router.js
+++ app/src/router.js
@@ -1,7 +1,7 @@
 var page = require('page')
 var logger = require('./modules/Logger')
 
-page('*', require('./modules/page.logger'), require('./modules/page.tracker'), require('./modules/page.scrollToTop'))
+page('*', require('./modules/page.logger'), require('./modules/page.tracker'), require('./modules/page.scrollToTop')(['statistics']))
 page('/', require('./pages/Main'))
 page('/about', require('./pages/About'))
 page('/statistics', require('./pages/Statistics'))
