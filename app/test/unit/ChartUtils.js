7589fe2e3b2f390261328c25c9c23c3fe73ada0c
Christian Fei <crifei93@gmail.com>
2015-06-08 19:46:09 +0200

    covering getPieChartDataFrom for simplest case

--- app/test/unit/ChartUtils.js
+++ app/test/unit/ChartUtils.js
@@ -3,7 +3,24 @@ var ChartUtils = require('../../src/modules/ChartUtils')
 var expect = require('chai').expect
 
 describe('ChartUtils', function () {
-  it('fails', function () {
-    expect(42).to.be.false
+  it('gets chart data for pie chart', function () {
+    var data = [{
+      type: 'pomodoro',
+      minutes: 25,
+      startedAt: Date.now()
+    }]
+    var expectedChartData = [{
+      value: 25,
+      color: '#DF2E2E',
+      highlight: '#DF2E2E',
+      label: 'Pomodori'
+    }, {
+      value: 0,
+      color: '#24b524',
+      highlight: '#24b524',
+      label: 'Breaks'
+    }]
+    var chartData = ChartUtils.getPieChartDataFrom(data)
+    expect( chartData ).to.deep.equal(expectedChartData)
   })
 })
