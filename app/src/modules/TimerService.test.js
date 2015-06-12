d1806651797bf6e3e054e0ce52345a38bb4495d6
Christian Fei <crifei93@gmail.com>
2015-06-12 21:13:04 +0200

    added beforeEach to require TimerService, to simulate that it is a singleton

--- app/src/modules/TimerService.test.js
+++ app/src/modules/TimerService.test.js
@@ -1,9 +1,13 @@
-var TimerService = require('./TimerService')
+var TimerService
 
 var expect = require('chai').expect
 
 
 describe('TimerService', function () {
+  beforeEach(function () {
+    TimerService = require('./TimerService')
+  })
+
   it('starts a timer', function () {
     expect( TimerService.start() ).to.be.ok
   })
