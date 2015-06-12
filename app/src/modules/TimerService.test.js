0e2dab07da3333680c7e7ca38a9760bb2ed835ae
Christian Fei <crifei93@gmail.com>
2015-06-12 21:00:56 +0200

    init TimerService with tests

---
+++ app/src/modules/TimerService.test.js
@@ -0,0 +1,10 @@
+var TimerService = require('./TimerService')
+
+var expect = require('chai').expect
+
+
+describe('TimerService', function () {
+  it('succeeds', function () {
+    expect(TimerService).to.be.ok
+  })
+})
