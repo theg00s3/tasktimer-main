75878951663ec4a8eb20264bb6eeaa2272273b72
Christian Fei <crifei93@gmail.com>
2015-06-13 13:34:47 +0200

    organized Timer tests in behaviour suite

--- app/src/modules/Timer.test.js
+++ app/src/modules/Timer.test.js
@@ -19,31 +19,32 @@ describe('Timer', function () {
     clock.restore()
   })
 
+  describe('behaviour', function () {
+    it('refuses to start a timer with invalid seconds', function () {
+      expect( Timer.start(123.2) ).not.to.be.ok
+      expect( Timer.start(-1) ).not.to.be.ok
+    })
 
-  it('refuses to start a timer with invalid seconds', function () {
-    expect( Timer.start(123.2) ).not.to.be.ok
-    expect( Timer.start(-1) ).not.to.be.ok
-  })
+    it('starts a timer with given seconds', function () {
+      expect( Timer.start(25*60) ).to.be.ok
+    })
 
-  it('starts a timer with given seconds', function () {
-    expect( Timer.start(25*60) ).to.be.ok
-  })
+    it('refuses to start another timer when one is in progress', function () {
+      Timer.start(25*60)
+      expect( Timer.start(25*60) ).not.to.be.ok
+    })
 
-  it('refuses to start another timer when one is in progress', function () {
-    Timer.start(25*60)
-    expect( Timer.start(25*60) ).not.to.be.ok
-  })
+    it('stops a timer', function () {
+      Timer.start(25*60)
+      expect( Timer.stop() ).to.be.ok
+    })
 
-  it('stops a timer', function () {
-    Timer.start(25*60)
-    expect( Timer.stop() ).to.be.ok
-  })
-
-  it('returns the remaining time', function () {
-    expect( Timer.start(25*60) ).to.be.ok
-    clock.tick(1)
-    expect( Timer.getRemaining() ).to.be.ok
-    expect( Timer.getRemaining() ).to.eql( 25*60 -1 )
+    it('returns the remaining time', function () {
+      expect( Timer.start(25*60) ).to.be.ok
+      clock.tick(1)
+      expect( Timer.getRemaining() ).to.be.ok
+      expect( Timer.getRemaining() ).to.eql( 25*60 -1 )
+    })
   })
 
 })
