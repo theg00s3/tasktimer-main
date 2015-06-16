32ecee5b203affeeb397bfd5b2fdcc9fa83f29b7
Christian Fei <crifei93@gmail.com>
2015-06-17 00:18:42 +0200

    tmp added Timeline

--- app/src/components/Timeline.js
+++ app/src/components/Timeline.js
@@ -1,8 +1,20 @@
 var React = require('react')
+  , moment = require('moment')
   , PomodoroUtils = require('../../../shared/PomodoroUtils')
 
 module.exports = React.createClass({
   render: function(){
-    return null
+    var timeline = this.props.data.map(function(dataPoint){
+      var formattedStartedAt = moment(dataPoint.startedAt).format('HH:mm')
+      var duration = PomodoroUtils.getDurationInMinutes(dataPoint)
+      return  <li className={"type-"+dataPoint.type}>
+                <span className="duration">Duration: {duration}min</span>
+                <span className="startedAt">{formattedStartedAt}</span>
+              </li>
+    })
+
+    return <ul className="timeline">
+              {timeline}
+            </ul>
   }
 })
