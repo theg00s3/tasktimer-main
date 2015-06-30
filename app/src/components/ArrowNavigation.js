ca92e92a6479b672a6ec4c151b29b8d5e05252b6
Christian Fei <christian.fei@xpeppers.com>
2015-07-01 00:54:39 +0200

    made statistics navigation arrows  clickable links

--- app/src/components/ArrowNavigation.js
+++ app/src/components/ArrowNavigation.js
@@ -2,9 +2,13 @@ var React = require('react')
 
 module.exports = React.createClass({
   render: function(){
-    return  <div className="arrow-navigation" role="navigation">
-              <i onClick={this.props.onBack} className="icon ion-ios-arrow-back"></i>
-              <i onClick={this.props.onForward} className="icon ion-ios-arrow-forward"></i>
-            </div>
+  return  <div className="arrow-navigation" role="navigation">
+        <a href="" onClick={this.props.onBack}>
+          <i className="icon ion-ios-arrow-back"></i>
+        </a>
+        <a href="" onClick={this.props.onForward}>
+          <i className="icon ion-ios-arrow-forward"></i>
+        </a>
+      </div>
   }
 })
