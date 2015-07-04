33cff13c7a481e06f7c5a4718f64f5ba41605b6d
Christian Fei <christian.fei@xpeppers.com>
2015-07-04 17:06:50 +0200

    refactored TimerService.test with beforeEach hook

--- app/src/modules/TimerService.test.js
+++ app/src/modules/TimerService.test.js
@@ -1,10 +1,13 @@
-var TimerService = require('./TimerService')
+var TimerService
   , Timer = require('./Timer')
 
 var expect = require('chai').expect
   , sinon = require('sinon')
 
 describe('TimerService', function () {
+  beforeEach(function () {
+    TimerService = require('./TimerService')
+  })
   it('subscribes to Timer on tick event', function () {
     var mockTimer = sinon.mock(Timer)
     mockTimer.expects('on').once().withArgs('tick').returns(Timer)
