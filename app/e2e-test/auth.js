1ed53105aae88091a81d152a22cc641965c86011
Christian Fei <crifei93@gmail.com>
2015-07-18 15:37:51 +0200

    fixed e2e test for fake auth

--- app/e2e-test/auth.js
+++ app/e2e-test/auth.js
@@ -4,7 +4,8 @@ module.exports = {
   },
   'adds changes color of header when navigated to statistics page': function(browser){
     browser
-      .url('https://pomodoro.dev/')
+      .url('https://pomodoro.dev/auth/fake')
+      
     browser
       .assert.attributeContains('.user-profile', 'src', 'https://avatars.githubusercontent.com/u/2662706?v=3')
   },
