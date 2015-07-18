10407c787ca71fad2cae7b41e04c8ba64693b787
Christian Fei <crifei93@gmail.com>
2015-07-18 17:09:58 +0200

    added e2e test for not showing login-logout on statistics page if logged in

--- app/e2e-test/auth.js
+++ app/e2e-test/auth.js
@@ -2,11 +2,18 @@ module.exports = {
   after: function(browser){
     browser.end()
   },
-  'adds changes color of header when navigated to statistics page': function(browser){
+  before: function (browser) {
     browser
       .url('https://pomodoro.dev/auth/fake')
-      
+  },
+  'shows user information if logged in': function(browser){
     browser
+      .url('https://pomodoro.dev/')
       .assert.attributeContains('.user-profile', 'src', 'https://avatars.githubusercontent.com/u/2662706?v=3')
   },
+  'show statistics to the logged in user': function(browser){
+    browser
+      .url('https://pomodoro.dev/statistics')
+      .assert.elementNotPresent('.login-logout.big')
+  }
 }
