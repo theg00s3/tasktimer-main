107a35639dabd9ed07f1862a3a68ad27d287f1ca
Christian Fei <crifei93@gmail.com>
2015-07-18 17:12:38 +0200

    added test for showing timeline when day has data, updated circle to seed DB

--- app/e2e-test/auth.js
+++ app/e2e-test/auth.js
@@ -6,14 +6,19 @@ module.exports = {
     browser
       .url('https://pomodoro.dev/auth/fake')
   },
-  'shows user information if logged in': function(browser){
+  'shows user information in the header': function(browser){
     browser
       .url('https://pomodoro.dev/')
       .assert.attributeContains('.user-profile', 'src', 'https://avatars.githubusercontent.com/u/2662706?v=3')
   },
-  'show statistics to the logged in user': function(browser){
+  'does not show login prompt': function(browser){
     browser
       .url('https://pomodoro.dev/statistics')
       .assert.elementNotPresent('.login-logout.big')
-  }
+  },
+  'shows statistcs for the selected day': function(browser){
+    browser
+      .url('https://pomodoro.dev/statistics?day=07/12/2015')
+      .assert.elementPresent('.timeline-container')
+  },
 }
