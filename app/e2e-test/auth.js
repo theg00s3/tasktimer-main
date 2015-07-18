5094fe1132e7ff7e22457c066eb999aefa936fdf
Christian Fei <crifei93@gmail.com>
2015-07-18 15:37:10 +0200

    added test for fake auth, improved test.docker.sh

---
+++ app/e2e-test/auth.js
@@ -0,0 +1,11 @@
+module.exports = {
+  after: function(browser){
+    browser.end()
+  },
+  'adds changes color of header when navigated to statistics page': function(browser){
+    browser
+      .url('https://pomodoro.dev/')
+    browser
+      .assert.attributeContains('.user-profile', 'src', 'https://avatars.githubusercontent.com/u/2662706?v=3')
+  },
+}
