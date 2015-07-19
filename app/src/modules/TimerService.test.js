09cbcbfee7d895e16ed0ab8d1330fc6cd8196814
Christian Fei <crifei93@gmail.com>
2015-07-19 18:38:29 +0200

    refactoring TimerService.tests

--- app/src/modules/TimerService.test.js
+++ app/src/modules/TimerService.test.js
@@ -1,25 +1,18 @@
 var expect = require('chai').expect
   , sinon = require('sinon')
 
-var TimerService
+var TimerService = require('./TimerService')
   , Timer = require('./Timer')
-  , MockTimer = sinon.mock(Timer)
   , FakeTimer = require('../../fixtures/FakeTimer')
   , VoidTimer = require('../../fixtures/VoidTimer')
   , StartTimer = require('../../fixtures/StartTimer')
   , EndTimer = require('../../fixtures/EndTimer')
   , DocumentTitleUpdateCommand = require('./DocumentTitleUpdateCommand')
-  , MockDocumentTitleUpdateCommand = sinon.mock(DocumentTitleUpdateCommand)
 
 
 describe('TimerService', function () {
-  beforeEach(function () {
-    TimerService = require('./TimerService')
-    MockTimer.restore()
-    MockDocumentTitleUpdateCommand.restore()
-  })
-
   it('subscribes to Timer events', function () {
+    var MockTimer = sinon.mock(Timer)
     MockTimer.expects('on').once().withArgs('tick').returns(Timer)
     MockTimer.expects('on').once().withArgs('end').returns(Timer)
     MockTimer.expects('on').once().withArgs('start').returns(Timer)
@@ -27,13 +20,16 @@ describe('TimerService', function () {
     TimerService.start(Timer)
 
     MockTimer.verify()
+    MockTimer.restore()
   })
 
   it('calls the collaborator DocumentTitleUpdateCommand.execute with remaining time', function () {
+    var MockDocumentTitleUpdateCommand = sinon.mock(DocumentTitleUpdateCommand)
     MockDocumentTitleUpdateCommand.expects('execute').once().withArgs(25*60)
 
     TimerService.start(FakeTimer, DocumentTitleUpdateCommand)
 
     MockDocumentTitleUpdateCommand.verify()
+    MockDocumentTitleUpdateCommand.restore()
   })
 })
