688a0719d847bca93e5d1a88ff76e4f8133b09cc
Christian Fei <crifei93@gmail.com>
2015-07-24 22:04:30 +0200

    removed dropdown, added navigation buttons in header

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,20 +1,23 @@
 var React = require('react')
-  , Dropdown = require('./Dropdown')
   , UserProfile = require('./UserProfile')
   , LoginLogout = require('./LoginLogout')
 
 module.exports = React.createClass({
   render: function() {
-    var items = [
-      <li key="1"><a href="/">Dashboard</a></li>,
-      <li key="2"><a href="/statistics">Statistics</a></li>,
-      <li key="3"><a href="/blog">Blog</a></li>,
-      <li key="4"><a href="/about">About</a></li>,
-    ]
     return  <header role="header">
               <div className="limit">
                 <a href="/" tabIndex="1" className="brand">p<span className="rest">omodoro</span><span className="tld">.cc</span></a>
-                <Dropdown text="Menu" items={items} tabIndex="2"/>
+                <ul className="menu">
+                  <li>
+                    <a href="/statistics" tabIndex="2">Stats</a>
+                  </li>
+                  <li>
+                    <a href="/blog" tabIndex="3">Blog</a>
+                  </li>
+                  <li>
+                    <a href="/about" tabIndex="4">About</a>
+                  </li>
+                </ul>
                 <LoginLogout/>
                 <UserProfile/>
               </div>
