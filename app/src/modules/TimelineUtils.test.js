789ab639af533ddcd182ea33cfb3aea23e0bc2bf
Christian Fei <crifei93@gmail.com>
2015-07-25 12:54:21 +0200

    refactoring TimelineUtils tests

--- app/src/modules/TimelineUtils.test.js
+++ app/src/modules/TimelineUtils.test.js
@@ -5,55 +5,52 @@ var expect = require('chai').expect
 describe('TimelineUtils', function () {
   var data = require('../../fixtures/timelineData.json')
 
-  it('returns undefined for an empty list', function () {
-    expect( TimelineUtils.calculateStart([]) ).to.eql( undefined )
+  describe('calculates start and end', function () {
+    it('undefined start time for an empty list', function () {
+      expect( TimelineUtils.calculateStart([]) ).to.eql( undefined )
+    })
+    it('start time for list', function () {
+      expect( TimelineUtils.calculateStart(data) ).to.eql( data[0].startedAt )
+    })
+    it('start time in hour format for list', function () {
+      expect( TimelineUtils.calculateStartHour(data) ).to.match( /(17\:00|15\:00)/ )
+    })
+    it('undefined end time for an empty list', function () {
+      expect( TimelineUtils.calculateEnd([]) ).to.eql( undefined )
+    })
+    it('end time for list', function () {
+      expect( TimelineUtils.calculateEnd(data) ).to.eql( data[data.length-1].startedAt )
+    })
+    it('end time in hour format for list', function () {
+      expect( TimelineUtils.calculateEndHour(data) ).to.match( /(18\:00|16\:00)/ )
+    })
+    it('start time for single item', function () {
+      expect( TimelineUtils.calculateStart(data[0]) ).to.eql( data[0].startedAt )
+    })
+    it('end time for single item', function () {
+      expect( TimelineUtils.calculateEnd(data[0]) ).to.eql( data[0].startedAt )
+    })
   })
 
-  it('calculates the start time', function () {
-    expect( TimelineUtils.calculateStart(data) ).to.eql( data[0].startedAt )
-  })
 
-  it('calculates the start time in hour format', function () {
-    expect( TimelineUtils.calculateStartHour(data) ).to.match( /(17\:00|15\:00)/ )
-  })
-
-  it('calculates the end time', function () {
-    expect( TimelineUtils.calculateEnd(data) ).to.eql( data[data.length-1].startedAt )
-  })
-
-  it('calculates the end time in hour format', function () {
-    expect( TimelineUtils.calculateEndHour(data) ).to.match( /(18\:00|16\:00)/ )
-  })
-
-  it('can be used to get the start time from a single item', function () {
-    expect( TimelineUtils.calculateStart(data[0]) ).to.eql( data[0].startedAt )
-  })
-
-  it('can be used to get the end time from a single item', function () {
-    expect( TimelineUtils.calculateEnd(data[0]) ).to.eql( data[0].startedAt )
-  })
-
-  describe('timeline rendering data', function () {
-    it('calculates the horizontal position', function () {
+  describe('calculates timeline rendering data', function () {
+    it('horizontal position', function () {
       expect( TimelineUtils.calculateTimelineItem(data[0], data).x ).to.eql( 208.3 )
       expect( TimelineUtils.calculateTimelineItem(data[1], data).x ).to.eql( 451.5 )
       expect( TimelineUtils.calculateTimelineItem(data[2], data).x ).to.eql( 616.9 )
       expect( TimelineUtils.calculateTimelineItem(data[3], data).x ).to.eql( 782.2 )
     })
-
-    it('calculates the radius', function () {
+    it('radius', function () {
       expect( TimelineUtils.calculateTimelineItem(data[0], data).r ).to.eql( 208.3 )
       expect( TimelineUtils.calculateTimelineItem(data[1], data).r ).to.eql( 41.6 )
       expect( TimelineUtils.calculateTimelineItem(data[2], data).r ).to.eql( 125 )
       expect( TimelineUtils.calculateTimelineItem(data[3], data).r ).to.eql( 208.3 )
     })
-
-    it('calculates the className', function () {
+    it('className', function () {
       expect( TimelineUtils.calculateTimelineItem(data[0], data).className ).to.eql( data[0].type )
       expect( TimelineUtils.calculateTimelineItem(data[1], data).className ).to.eql( data[1].type )
       expect( TimelineUtils.calculateTimelineItem(data[2], data).className ).to.eql( data[2].type )
       expect( TimelineUtils.calculateTimelineItem(data[3], data).className ).to.eql( data[3].type )
     })
   })
-
 })
