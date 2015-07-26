36d487aa2654a0d714f593953b7f6d9c9325dd3e
Christian Fei <crifei93@gmail.com>
2015-07-26 23:08:23 +0200

    cleaned up StatisticsNumbers component

--- app/src/components/StatisticsNumbers.js
+++ app/src/components/StatisticsNumbers.js
@@ -3,20 +3,37 @@ var React = require('react')
 var StatisticsUtils = require('../modules/StatisticsUtils')
 
 module.exports = React.createClass({
+  getInitialState: function(){
+    return {
+      fullPomodoroCount: 0,
+      partialPomodoroCount: 0,
+      fullPomodoroHours: 0,
+    }
+  },
+  componentDidMount: function(){
+    var fullPomodoroCount = StatisticsUtils.getFullPomodoroCount(this.props.data)
+    var partialPomodoroCount = StatisticsUtils.getPartialPomodoroCount(this.props.data) - fullPomodoroCount
+    var fullPomodoroHours = StatisticsUtils.getFullPomodoroHours(this.props.data)
+    this.setState({
+      fullPomodoroCount: fullPomodoroCount,
+      partialPomodoroCount: partialPomodoroCount,
+      fullPomodoroHours: fullPomodoroHours,
+    })
+  },
   render: function(){
     return  <div className="statistics-numbers-container">
               <div>
                 <div>
-                  <span className="number">{StatisticsUtils.getPartialPomodoroCount(this.props.data)}</span>
+                  <span className="number">{this.state.fullPomodoroCount}</span>
                   <span className="text">pomodori</span>
                   <br/>
                   <span className="small">
-                    of which <strong>{StatisticsUtils.getPartialPomodoroCount(this.props.data) - StatisticsUtils.getFullPomodoroCount(this.props.data)}</strong> are partial
+                    of which <strong>{this.state.partialPomodoroCount}</strong> are partial
                   </span>
                 </div>
               </div>
               <div>
-                <span className="number">{StatisticsUtils.getFullPomodoroHours(this.props.data)}</span>
+                <span className="number">{this.state.fullPomodoroHours}</span>
                 <span className="text">hours</span>
               </div>
             </div>
