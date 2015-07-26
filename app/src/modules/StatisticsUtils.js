eecf33ace430b0e05d3494d75f45d59de7660ea4
Christian Fei <crifei93@gmail.com>
2015-07-26 14:45:19 +0200

    added test to get full pomodoro count for data in StatisticsUtils

--- app/src/modules/StatisticsUtils.js
+++ app/src/modules/StatisticsUtils.js
@@ -1,7 +1,16 @@
 module.exports = {
-  getPomodoroCount: getPomodoroCount
+  getFullPomodoroCount: getFullPomodoroCount
 }
 
-function getPomodoroCount(){
-  return 2
+var _ = require('underscore')
+
+function getFullPomodoroCount(data){
+  return _.reduce(data, function(acc, pomodoro){
+    var fullPomodoro = isFullPomodoro(pomodoro)
+    return acc + (fullPomodoro ? 1 : 0)
+  }, 0)
+}
+
+function isFullPomodoro(pomodoro){
+  return 'pomodoro' === pomodoro.type && pomodoro.cancelledAt === undefined
 }
--- app/src/modules/StatisticsUtils.test.js
+++ app/src/modules/StatisticsUtils.test.js
@@ -1,12 +1,26 @@
 var StatisticsUtils = require('./StatisticsUtils')
 
 var expect = require('chai').expect
+var _ = require('underscore')
 
 describe('StatisticsUtils', function () {
-  var data = require('../../fixtures/timelineData.json')
+  var data
+  beforeEach(function () {
+    var _data = require('../../fixtures/timelineData')
+    data = _.clone(_data)
+  })
 
-  it('calculates pomodoro count', function () {
-    var pomodoroCount = StatisticsUtils.getPomodoroCount(data)
+  it('calculates full pomodoro count', function () {
+    var pomodoroCount = StatisticsUtils.getFullPomodoroCount(data)
+    expect( pomodoroCount ).to.eql( 2 )
+
+    data.push({
+      "startedAt": "Sat Jul 04 2015 18:00:00 GMT+0200 (CEST)",
+      "cancelledAt": "Sat Jul 04 2015 18:05:00 GMT+0200 (CEST)",
+      "minutes": 25,
+      "type": "pomodoro"
+    })
+    pomodoroCount = StatisticsUtils.getFullPomodoroCount(data)
     expect( pomodoroCount ).to.eql( 2 )
   })
 })
