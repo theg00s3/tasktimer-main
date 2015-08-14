e0e929d5bacea795617faf89b7ca3e6f91e061bf
Christian Fei <crifei93@gmail.com>
2015-08-14 22:28:55 +0200

    added bitcoin donate button in footer

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -15,6 +15,9 @@ module.exports = React.createClass({
                   </ul>
                 </div>
                 <div className="tac">
+                  <a className="coinbase-button" data-code="ab43631f12fb206ac3709f03e3b3e012" data-button-style="donation_small" href="https://www.coinbase.com/checkouts/ab43631f12fb206ac3709f03e3b3e012">Donate Bitcoins</a>
+                </div>
+                <div className="tac">
                   <p>
                     <strong>by</strong> <a href="http://christian.fei.ninja" target="_blank">Christian Fei</a>
                   </p>
