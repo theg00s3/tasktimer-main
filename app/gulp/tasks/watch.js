f4c15375ce49695477c0935bf000d4330d20165f
Christian Fei <crifei93@gmail.com>
2015-08-15 15:23:43 +0200

    build in watch task

--- app/gulp/tasks/watch.js
+++ app/gulp/tasks/watch.js
@@ -3,7 +3,7 @@ var gulp = require('gulp')
   , watchify = require('watchify')
   , gutil = require('gulp-util')
 
-gulp.task('watch', ['watchify','browser-sync'], function() {
+gulp.task('watch', ['watchify','default','browser-sync'], function() {
   gulp.watch(config.paths.test, ['test'])
   gulp.watch(config.paths.stylus, ['stylus'])
   gulp.watch(config.paths.jade, ['jade'])
