a889ca9b91e9781794560019fd97a5b1ee28c465
Christian Fei <crifei93@gmail.com>
2015-08-17 00:15:39 +0200

    cleaned up styles

--- app/src/components/Alert.js
+++
@@ -1,9 +0,0 @@
-var React = require('react')
-
-module.exports = React.createClass({
-  render: function(){
-    return  <div className="alert alert-success limit small tac breath">
-              {this.props.children}
-            </div>
-  }
-})
