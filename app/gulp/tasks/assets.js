92bcb8837d131fc174e3e9698577fde0808ceb5d
Christian Fei <crifei93@gmail.com>
2015-08-20 23:20:21 +0200

    added basic sitemap and gulp task

--- app/gulp/tasks/assets.js
+++ app/gulp/tasks/assets.js
@@ -5,6 +5,8 @@ var gulp = require('gulp')
 gulp.task('assets', function(){
   gulp.src('src/favicon.ico')
     .pipe(gulp.dest('www/'))
+  gulp.src('src/sitemap.xml')
+    .pipe(gulp.dest('www/'))
   gulp.src(config.paths.assets)
     .pipe(gulp.dest('www/assets/'))
     .pipe(browserSync.reload({stream:true}))
