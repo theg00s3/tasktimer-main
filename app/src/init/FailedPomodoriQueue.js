f643a0e02d8765b59a723243ca28ae6dec3da060
Christian Fei <crifei93@gmail.com>
2015-08-29 16:54:12 +0200

    improved retry queue

--- app/src/init/FailedPomodoriQueue.js
+++ app/src/init/FailedPomodoriQueue.js
@@ -4,29 +4,20 @@ var failedPomodoriKey = require('../constants').failedPomodoriKey
 var failedPomodoriQueue = new PersistentQueue(failedPomodoriKey)
 
 module.exports = function(){
-  trySaveFailedPomodori()
-  setInterval(function(){
-    if( failedPomodoriQueue.hasItems() ){
-      trySaveFailedPomodori()
-    }
-  }, 1 * 1000 * 60)
+  retryNextFailedPomodoro()
 }
 
-function trySaveFailedPomodori(){
-  var savingFailed = false
-  while( !savingFailed && failedPomodoriQueue.hasItems() ){
+function retryNextFailedPomodoro(){
+  if( failedPomodoriQueue.hasItems() ){
     var pomodoro = failedPomodoriQueue.pop()
     trySavedFailedPomodoro(pomodoro)
-    .catch(function(){
-      savingFailed = true
-    })
+    .then(retryNextFailedPomodoro)
   }
 }
 
-function trySavedFailedPomodoro(pomodoro, failedCallback){
+function trySavedFailedPomodoro(pomodoro){
   return PomodoroRepository.create(pomodoro)
   .catch(function(){
     failedPomodoriQueue.push(pomodoro)
-    failedCallback()
   })
 }
