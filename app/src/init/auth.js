0366380642d5055be5e21cefafbb24929db0c1cf
Christian Fei <crifei93@gmail.com>
2015-08-29 16:55:15 +0200

    removed 'login debug' and 'login error' tracking event

--- app/src/init/auth.js
+++ app/src/init/auth.js
@@ -5,13 +5,9 @@ module.exports = function(){
   return AuthService.authenticate()
     .then(function(response){
       var user = response.data
-      AnalyticsService.track('login-debug', user)
       AnalyticsService.identify(user.id, {
         name: user.username,
         username: user.username,
       })
-    })
-    .catch(function(){
-      AnalyticsService.track('login error', arguments)
     })
 }
