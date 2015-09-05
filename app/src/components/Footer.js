b6d16fc1d91a929558dc955e2df91f325b676d66
Christian Fei <crifei93@gmail.com>
2015-09-05 09:43:42 +0200

    updated footer layout

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -18,21 +18,18 @@ module.exports = React.createClass({
                   </ul>
                 </div>
                 <div className="tac">
-                  <a className="coinbase-button" data-code="ab43631f12fb206ac3709f03e3b3e012" data-button-style="donation_small" href="https://www.coinbase.com/checkouts/ab43631f12fb206ac3709f03e3b3e012">Donate Bitcoins</a>
-                </div>
-                <div className="tac">
-                  <p>
+                  <p className="inline">
                     <strong>by</strong> <a href="http://christian.fei.ninja" target="_blank">Christian Fei</a>
                   </p>
-                  <p>
+                  <p className="inline">
+                    <strong>As seen on&nbsp;</strong>
+                    <a href="http://www.producthunt.com/e/productivity-hacks" target="_blank">
+                      <img width="120px" className="producthunt-logo" src="/assets/img/ph_logo.png" alt="As seen on ProductHunt"></img>
+                    </a>
+                  </p>
+                  <p className="inline">
                     <strong>hosted on</strong> <a href="https://www.digitalocean.com/?refcode=880e8f681b50">digitalocean</a>
                   </p>
-                </div>
-                <div className="tac">
-                  <strong>As seen on&nbsp;</strong>
-                  <a href="http://www.producthunt.com/e/productivity-hacks" target="_blank">
-                    <img width="120px" className="producthunt-logo" src="/assets/img/ph_logo.png" alt="As seen on ProductHunt"></img>
-                  </a>
                 </div>
                 <div className="tac">
                   <p className="small light">
@@ -40,9 +37,14 @@ module.exports = React.createClass({
                   </p>
                 </div>
                 <div className="tac">
-                  <a href="https://mixpanel.com/f/partner">
-                    <img src="//cdn.mxpnl.com/site_media/images/partner/badge_light.png" alt="Mobile Analytics"/>
-                  </a>
+                  <p className="inline">
+                    <a href="https://mixpanel.com/f/partner">
+                      <img src="//cdn.mxpnl.com/site_media/images/partner/badge_light.png" alt="Mobile Analytics"/>
+                    </a>
+                  </p>
+                  <p className="inline vmiddle">
+                    <a className="coinbase-button" data-code="ab43631f12fb206ac3709f03e3b3e012" data-button-style="donation_small" href="https://www.coinbase.com/checkouts/ab43631f12fb206ac3709f03e3b3e012">Donate Bitcoins</a>
+                  </p>
                 </div>
               </div>
             </footer>
