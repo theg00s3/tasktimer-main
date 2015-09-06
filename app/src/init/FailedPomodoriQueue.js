bc9ed8cd8a1a185ac788a1bd2074a31b40d16ae9
Christian Fei <crifei93@gmail.com>
2015-09-06 11:05:28 +0200

    improved FailedPomodoroQueue, retrying only if not overlapping or invalid pomodoro error code

--- app/src/init/FailedPomodoriQueue.js
+++ app/src/init/FailedPomodoriQueue.js
@@ -1,17 +1,21 @@
 var PersistentQueue = require('../modules/PersistentQueue')
 var PomodoroRepository = require('../modules/PomodoroRepository')
+var AuthService = require('../modules/AuthService')
 var failedPomodoriKey = require('../constants').failedPomodoriKey
 var failedPomodoriQueue = new PersistentQueue(failedPomodoriKey)
 
 module.exports = function(){
-  retryNextFailedPomodoro()
+  AuthService.authenticate()
+  .then(retryNextFailedPomodoro)
 }
 
 function retryNextFailedPomodoro(){
   if( failedPomodoriQueue.hasItems() ){
     var pomodoro = failedPomodoriQueue.pop()
     trySavedFailedPomodoro(pomodoro)
-    .then(retryNextFailedPomodoro)
+    .then(function(){
+      setTimeout(retryNextFailedPomodoro, 500)
+    })
   }
 }
 
