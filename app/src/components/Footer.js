45a31436e397936a380931482168fb9ab7e71d7a
Christian Fei <crifei93@gmail.com>
2015-09-30 22:08:29 +0200

    removed coinbase

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -42,9 +42,6 @@ module.exports = React.createClass({
                       <img src="//cdn.mxpnl.com/site_media/images/partner/badge_light.png" alt="Mobile Analytics"/>
                     </a>
                   </p>
-                  <p className="inline vmiddle">
-                    <a className="coinbase-button" data-code="ab43631f12fb206ac3709f03e3b3e012" data-button-style="donation_small" href="https://www.coinbase.com/checkouts/ab43631f12fb206ac3709f03e3b3e012">Donate Bitcoins</a>
-                  </p>
                 </div>
               </div>
             </footer>
