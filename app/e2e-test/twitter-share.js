7baac056df1ee549802573360a11eed81a5b39fe
Christian Fei <crifei93@gmail.com>
2015-10-23 23:26:49 +0200

    added test twitter share, updated styles

---
+++ app/e2e-test/twitter-share.js
@@ -0,0 +1,25 @@
+module.exports = {
+  beforeEach: function(browser){
+    browser
+      .url('https://pomodoro.dev')
+      .waitForElementVisible('main', 1000)
+  },
+  after: function(browser){
+    browser.end()
+  },
+  "shares tweet": function(browser){
+    browser
+      .click('.twitter-share')
+      .pause(1500)
+      .getText('body', containsRegExp(/#productivity/))
+  },
+}
+
+
+function containsRegExp(regexp){
+  return function(result){
+    var text = result.value
+    if( !text )return
+    this.assert.ok(regexp.test(text))
+  }
+}
