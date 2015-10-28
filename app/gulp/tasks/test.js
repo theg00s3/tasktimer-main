ecb946b59eb95c3174cde76d367f3493e7c7dd79
Christian Fei <crifei93@gmail.com>
2015-10-28 19:59:01 +0100

    updated test task

--- app/gulp/tasks/test.js
+++ app/gulp/tasks/test.js
@@ -1,21 +1,32 @@
-var config = require('../config')
 var gulp = require('gulp')
-var karma = require('gulp-karma')
+var Server = require('karma').Server
 
-var PRODUCTION = !!process.env.PRODUCTION
+gulp.task('test', function (done) {
+  new Server({
+    configFile: process.cwd() + '/karma.conf.js',
+    singleRun: true
+  }, done).start()
+})
 
-function test(done){
-  return gulp.src(config.paths.test)
-    .pipe(karma({
-      configFile: process.cwd() + '/karma.conf.js',
-      action: 'run'
-    }))
-    .on('error', function(err) {
-      this.emit('end')
-      if( PRODUCTION ){throw err}
-    })
-}
 
-gulp.task('test', test)
+// var config = require('../config')
+// var gulp = require('gulp')
+// var karma = require('gulp-karma')
+
+// var PRODUCTION = !!process.env.PRODUCTION
+
+// function test(done){
+//   return gulp.src(config.paths.test)
+//     .pipe(karma({
+//       configFile: process.cwd() + '/karma.conf.js',
+//       action: 'run'
+//     }))
+//     .on('error', function(err) {
+//       this.emit('end')
+//       if( PRODUCTION ){throw err}
+//     })
+// }
+
+// gulp.task('test', test)
 
 module.exports = test
