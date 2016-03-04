034fc0f41a78d4c90c6b5941bb1e202ac9872820
Christian Fei <crifei93@gmail.com>
2016-03-05 00:59:43 +0100

    added UAT for docs

--- app/src/components/MainFooter.js
+++ app/src/components/MainFooter.js
@@ -9,6 +9,9 @@ export default class MainFooter extends Component {
                   Follow us on <a href="https://twitter.com/pomodoro_cc" target="_blank">Twitter</a> for the latest news! Also give us feedback and contact us for feature requests!
                 </p>
                 <p>
+                  Check out the <a href="/docs" id="docs-link">developer documentation</a>!
+                </p>
+                <p>
                   For feedback, bug reports or pull requests, please use <a href="https://github.com/christian-fei/pomodoro.cc/" target="_blank">Github</a> or contact us on <a href="https://twitter.com/pomodoro_cc" target="_blank">Twitter</a>
                 </p>
                 <p>
