0a90f3a373b42090e32ce851a8e092f8da68525a
Christian Fei <crifei93@gmail.com>
2017-10-08 19:09:30 +0200

    update close button welcomebar

--- app/src/components/WelcomeBar.js
+++ app/src/components/WelcomeBar.js
@@ -9,7 +9,7 @@ export default class WelcomeBar extends Component {
     }
     return <div className='welcome-bar-container'>
       <div className='welcome-bar tac'>
-        <i onClick={() => actions.acknowledgeWelcome()} style={{'float': 'right'}} className='icon ion-close-round' >x</i>
+        <small><i onClick={actions.acknowledgeWelcome} style={{'float': 'right'}} className='icon ion-close-round' tabindex='0'>[x]</i></small>
         <h2 className='no-mt'>Welcome to pomodoro.cc!</h2>
         <p>We can help you to plan your activities for the day, and get things done!</p>
         <p>Pomodoro.cc is a tool that leverages the concepts of the <a href='http://pomodorotechnique.com/' target='_blank'>Pomodoro Technique</a> to help you to keep your focus with mind-refreshing breaks.</p>
