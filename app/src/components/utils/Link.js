5f447254896175c1ae158bcd6021139735a8a747
Christian Fei <crifei93@gmail.com>
2017-10-08 11:03:33 +0200

    add enter key support for Link

--- app/src/components/utils/Link.js
+++ app/src/components/utils/Link.js
@@ -3,13 +3,17 @@ import React, {Component} from 'react'
 export default class Link extends Component {
   render () {
     const url = this.props.to || this.props.href || this.props.url
-    return <span onClick={handleRoute} class='link'>{this.props.children}</span>
+    return <span tabindex='0' onKeyUp={withEnterKey(handleRoute)} onClick={handleRoute} class='link'>{this.props.children}</span>
 
     function handleRoute (event) {
       const popStateEvent = new window.PopStateEvent('popstate', {url})
       popStateEvent.url = url
       window.history.pushState({url}, null, url)
       window.dispatchEvent(popStateEvent)
+    }
+
+    function withEnterKey (callback) {
+      return (event) => (event.keyCode === 13) ? callback(event) : null
     }
   }
 }
