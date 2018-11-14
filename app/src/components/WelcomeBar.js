cf55f82fbf57d29ba6a54041c29b6d0340ee3056
Christian Fei <crifei93@gmail.com>
2018-11-14 21:45:41 +0100

    add close icon

--- app/src/components/WelcomeBar.js
+++ app/src/components/WelcomeBar.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import closeIcon from '../assets/images/close.png'
 import './WelcomeBar.styl'
 
 export default class WelcomeBar extends Component {
@@ -9,7 +10,9 @@ export default class WelcomeBar extends Component {
     }
     return <div className='welcome-bar-container'>
       <div className='welcome-bar tac'>
-        <small><i onClick={actions.acknowledgeWelcome} style={{'float': 'right'}} className='icon ion-close-round' tabindex='0'>[x]</i></small>
+        <div onClick={actions.acknowledgeWelcome} style={{'float': 'right'}} className='icon' tabindex='0'>
+          <img src={closeIcon} />
+        </div>
         <h2 className='no-mt'>Welcome to pomodoro.cc!</h2>
         <p>We can help you to plan your activities for the day, and get things done!</p>
         <p>Pomodoro.cc is a tool that leverages the concepts of the <a href='http://pomodorotechnique.com/' target='_blank'>Pomodoro Technique</a> to help you to keep your focus with mind-refreshing breaks.</p>
