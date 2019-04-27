4182eea14499ad06b7b02cf70eb6bade2d0b0411
Christian Fei <crifei93@gmail.com>
2019-04-28 00:00:04 +0200

    update patreon link

--- app/src/routes/Support.js
+++ app/src/routes/Support.js
@@ -7,13 +7,9 @@ export default class Support extends Component {
       <img className='support-image' src={logo} />
       <h1>Pomodoro.cc needs your support!</h1>
 
-      <h3 className='light1'>
-      Please consider becoming a backer on Patreon!
-      <br />
-        <br />
+      <h3 className='light1'>Please consider becoming a backer on Patreon!</h3>
 
-        <a href='https://www.patreon.com/bePatron?u=19605363' data-patreon-widget-type='become-patron-button'>Become a Patron!</a><script async src='https://c6.patreon.com/becomePatronButton.bundle.js' />
-      </h3>
+      <a href='https://www.patreon.com/bePatron?u=19605363' data-patreon-widget-type='become-patron-button'>Become a Patron!</a><script async src='https://c6.patreon.com/becomePatronButton.bundle.js' />
 
       <h3 className='light'>Every donation is very much appreciated.</h3>
       <h3 className='light'>With your support <strong>you</strong> can help us sustain the service costs and make it possible to continue to provide a free service.</h3>
