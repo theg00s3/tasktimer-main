cf5ef3dc16149888b47347ffd8f6a8f260c71caa
Christian Fei <crifei93@gmail.com>
2019-04-29 20:22:52 +0200

    add fourofour

--- app/src/routes/NotFound.js
+++ app/src/routes/NotFound.js
@@ -1,20 +1,15 @@
-import * as actions from '../actions'
 import React, {Component} from 'react'
-import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import Link from '../components/utils/Link'
 
-class FouroFour extends Component {
+class NotFound extends Component {
   render () {
     return <div className='content'>
       <div className='tac'>
-        <h1 className='giant'>404  🍅 not found</h1>
-        <Link to='/' className='action'>
-          <h1>
-            Start your first pomodoro!
-          </h1>
-        </Link>
-
+        <strong>Not Found</strong>
+      </div>
+      <div className='tac'>
+        <Link to='/'>Get productive</Link>
       </div>
     </div>
   }
@@ -28,6 +23,5 @@ export default connect(
     timer: state.timer
   }),
   (dispatch) => ({
-    actions: bindActionCreators(actions, dispatch)
   })
-)(FouroFour)
+)(NotFound)
