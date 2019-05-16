968863a2ee0575ecd6bb524f2bf2243ada949fa0
Christian Fei <crifei93@gmail.com>
2019-05-16 19:53:31 +0200

    about pomodoro technique

--- app/src/routes/About.js
+++ app/src/routes/About.js
@@ -7,10 +7,15 @@ export default class About extends Component {
       <div className='welcome-bar'>
         <h1 class='title'>Welcome to pomodoro.cc!</h1>
         <p>We can help you to plan your activities for the day, and get things done!</p>
+
         <p>
           Pomodoro.cc is a tool that leverages the concepts of the <a href='http://pomodorotechnique.com/' target='_blank'>Pomodoro Technique</a>
           <br />
           to help you to keep your focus with mind-refreshing breaks.
+        </p>
+
+        <p>
+          <strong>This application is not affiliated, associated or endorsed by the Pomodoro Technique® or Francesco Cirillo.</strong>
         </p>
 
         You can also <a href='https://www.patreon.com/pomodoro_cc' target='_blank'>support us on Patreon!</a>
