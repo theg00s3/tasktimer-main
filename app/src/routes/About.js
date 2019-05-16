a9b4b49e0e6706eb985376be6e7c404b2391d611
Christian Fei <crifei93@gmail.com>
2019-05-16 19:42:17 +0200

    improve open, about

--- app/src/routes/About.js
+++ app/src/routes/About.js
@@ -3,9 +3,9 @@ import './About.styl'
 
 export default class About extends Component {
   render () {
-    return <div className='welcome-bar-container'>
-      <div className='welcome-bar tac'>
-        <h2 className='no-mt'>Welcome to pomodoro.cc!</h2>
+    return <div className='content'>
+      <div className='welcome-bar'>
+        <h1 class='title'>Welcome to pomodoro.cc!</h1>
         <p>We can help you to plan your activities for the day, and get things done!</p>
         <p>
           Pomodoro.cc is a tool that leverages the concepts of the <a href='http://pomodorotechnique.com/' target='_blank'>Pomodoro Technique</a>
--- app/src/routes/Open.js
+++ app/src/routes/Open.js
@@ -5,7 +5,7 @@ export default class Open extends Component {
     return <div className='content open'>
       <h1>Open stats of pomodoro.cc</h1>
       <p>Data from <strong>Google Analytics</strong> and <strong>Google Data Studio</strong></p>
-      <iframe width='800' height='600' src='https://datastudio.google.com/embed/reporting/1YMIOWzefmW9Sf1dO3jfRnbV9uHbC1tuD/page/6Sbo' frameborder='0' style='border:0' allowfullscreen />
+      <iframe width='660' height='500' src='https://datastudio.google.com/embed/reporting/1YMIOWzefmW9Sf1dO3jfRnbV9uHbC1tuD/page/6Sbo' frameborder='0' style='border:0' allowfullscreen />
     </div>
   }
 }
