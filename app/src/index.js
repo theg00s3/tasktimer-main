2e3d67ae087d5ad601feb1da4964a1909de200c3
Christian Fei <crifei93@gmail.com>
2019-05-18 00:02:17 +0200

    add sentry to app

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,12 @@
-import Main from './Main'
 import React from 'react'
 import {render} from 'react-dom'
+import Main from './Main'
+import * as Sentry from '@sentry/browser'
 import init from './init'
 import './index.styl'
-// import 'normalize.css'
+Sentry.init({
+  dsn: 'https://4290a23ad5414f8693127ceba916795e@sentry.io/1462419'
+})
 
 init()
 render(<Main />, document.getElementById('main'))
