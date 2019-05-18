a9876983e01aee05ee3d2174884e7bb0c1a5e63f
Christian Fei <crifei93@gmail.com>
2019-05-18 09:30:46 +0200

    add productivity image

--- app/src/routes/Support.js
+++ app/src/routes/Support.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import './Support.styl'
+import Productivity from '../assets/images/productivity.jpeg'
 const logo = require('../assets/images/pomodoro.cc.png')
 
 export default class Support extends Component {
@@ -15,6 +16,8 @@ export default class Support extends Component {
       <h3 className='light'>Every donation is very much appreciated.</h3>
       <h3 className='light'>With your support <strong>you</strong> can help us sustain the service costs and make it possible to continue to provide a free service.</h3>
       <p>We strongly believe in distractions free work and thus decided <strong>to not show ads</strong>.</p>
+
+      <img src={Productivity} />
     </div>
   }
 }
