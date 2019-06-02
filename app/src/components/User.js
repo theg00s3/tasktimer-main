d8bee98ce9ad437bfb47a99852e151d454bd87f2
Christian Fei <crifei93@gmail.com>
2019-06-02 21:58:22 +0200

    fix path

--- app/src/components/User.js
+++ app/src/components/User.js
@@ -6,8 +6,12 @@ export default class User extends Component {
   render () {
     const {user} = this.props
     if (!user) return null
+    const href = `/user/${user.username}`
+
     return <span className='user'>
-      <img src={user.avatar} class='user-avatar' />
+      <Link href={href}>
+        <img src={user.avatar} class='user-avatar' />
+      </Link>
       <Link href='/logout'>Logout</Link>
     </span>
   }
