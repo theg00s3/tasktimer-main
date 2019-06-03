6e01598255fb232481a47c4430dac612c9bfc8df
Christian Fei <crifei93@gmail.com>
2019-06-03 22:08:58 +0200

    restore open

--- app/src/components/NavigationBar.js
+++ app/src/components/NavigationBar.js
@@ -13,7 +13,7 @@ export default class NavigationBar extends Component {
         {(!user || !user.subscription) && <Link className='pro' to='/pro'>Pro</Link>}
         <Link to='/team' id='team'>Team</Link>
         <Link to='/support'>Support us!</Link>
-        {/* <Link to='/open' id='open-stats'>Open</Link> */}
+        <Link to='/open' id='open-stats'>Open</Link>
         <Link to='/about' id='open-stats'>About</Link>
         <div class='float-right'>
           {!user && <Link to='/login'>Login</Link>}
