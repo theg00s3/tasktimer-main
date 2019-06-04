bf2aac3fc2409a3a6959442d785c4f4f6a7ebe79
Christian Fei <crifei93@gmail.com>
2019-06-04 21:51:22 +0200

    reeost open stats tests

--- app/cypress/integration/navigation.spec.js
+++ app/cypress/integration/navigation.spec.js
@@ -3,7 +3,7 @@
 context('Navigation', () => {
   beforeEach(() => cy.visit('http://localhost:1234'))
 
-  it.skip('shows open stats', () => {
+  it('shows open stats', () => {
     cy.get('#open-stats').click()
     cy.get('body').should('contain', 'Open stats of pomodoro.cc')
     cy.get('iframe')
