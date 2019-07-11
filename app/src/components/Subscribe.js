b3450bbf8b355d7a9b340643eca826787024069a
Christian Fei <crifei93@gmail.com>
2019-07-11 21:50:43 +0200

    update signup with images anad analysis

--- app/src/components/Subscribe.js
+++ app/src/components/Subscribe.js
@@ -3,10 +3,14 @@ import './Subscribe.styl'
 import SubscribeButton from './SubscribeButton'
 import Link from '../components/utils/Link'
 
+import AnalysisImage from '../assets/images/analysis.png'
+import StatisticsImage from '../assets/images/statistics.png'
+import DarkmodeImage from '../assets/images/darkmode.png'
+
 export default class Subscribe extends Component {
   render () {
     const {user, subscription, actions} = this.props
-    return <div className='content tac'>
+    return <div className='content1 tac'>
       <div className='subscribe'>
         <h1 className='title'>Pomodoro.cc <span className='pro-badge'>Pro</span></h1>
 
@@ -37,19 +41,28 @@ export default class Subscribe extends Component {
         <br />
         <br />
 
-        <ul className='pro-reasons tal'>
+        <ul className='pro-reasons tac'>
           <h1 className='title tac'>What you'll get</h1>
           <li className=''>
             ⚡️ Statistics about your days, weeks and months
+            <br />
+            <br />
+            <img src={StatisticsImage} alt='statistics' style='width: 100%; max-width: 700px' />
           </li>
           <li className=''>
             ⚡️ Analysis of your historic timers and todos
+            <br />
+            <br />
+            <img src={AnalysisImage} alt='analysis' style='width: 100%; max-width: 700px' />
           </li>
           <li className=''>
             ️️️⚡️ Sync data between devices
           </li>
           <li className=''>
             ️️️⚡️ Dark mode
+            <br />
+            <br />
+            <img src={DarkmodeImage} alt='darkmode' style='width: 100%; max-width: 700px' />
           </li>
           <li className=''>
             ️️️⚡️ Channel-reservation (Team Pomodoro)
