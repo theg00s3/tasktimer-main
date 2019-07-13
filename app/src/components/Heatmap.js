6ad2816c18d889aff9e3920039d2784e1dbfd484
Christian Fei <crifei93@gmail.com>
2019-07-13 15:24:59 +0200

    add info for stats heatmap

--- app/src/components/Heatmap.js
+++ app/src/components/Heatmap.js
@@ -3,14 +3,17 @@ import './Heatmap.styl'
 
 export default class Heatmap extends Component {
   render () {
-    const { analytics, onChangeDate = Function.prototype } = this.props
-    return <div className='heatmap'>
-      {analytics.map(a => {
-        return <span className='tooltip' onClick={() => onChangeDate(a.day)} title={a.day} data-value={a.pomodoros.length}>
-          &nbsp;
-          {/* <span className='tooltip-content'>{a.day} {a.pomodoros.length}</span> */}
-        </span>
-      })}
+    const { analytics, onChangeDate } = this.props
+    return <div className='heatmap-container'>
+      {onChangeDate && <strong>Click on any square below to see the daily stats</strong>}
+      <div className='heatmap'>
+        {analytics.map(a => {
+          return <span className='tooltip' onClick={() => onChangeDate && onChangeDate(a.day)} title={a.day} data-value={a.pomodoros.length}>
+            &nbsp;
+            {/* <span className='tooltip-content'>{a.day} {a.pomodoros.length}</span> */}
+          </span>
+        })}
+      </div>
     </div>
   }
 }
