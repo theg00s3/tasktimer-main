95b40896705718296c3f8608104b5b898a498ea5
Christian Fei <crifei93@gmail.com>
2019-07-13 14:25:47 +0200

    fix streak + improve calc

--- app/src/components/Streak.js
+++ app/src/components/Streak.js
@@ -4,30 +4,34 @@ import './Streak.styl'
 export default class Streak extends Component {
   render () {
     const {analysis} = this.props
-    const { longest, streaks } = analysis.reduce((streak, curr) => {
-      if (curr.pomodoros.length > 0) {
-        streak.current += 1
-      } else {
-        streak.longest = streak.current > streak.longest ? streak.current : streak.longest
-        streak.streaks.push(streak.longest)
-        streak.current = 0
-      }
-      return streak
-    }, {longest: 0, streaks: [], current: 0})
 
-    const averageStreaks = streaks.reduce((sum, s) => sum + s, 0) / Math.max(streaks.length, 1)
+    const { longest, average } = calc(analysis)
 
     return <div className='pad'>
       <div>
         <div className='columns'>
           <div className='column pad-v tac'>
-            <h1 className='no-m'>{longest}</h1> longest streak
+            <h1 className='no-m'>{longest.toFixed(0)}</h1> longest streak
           </div>
           <div className='column pad-v tac'>
-            <h1 className='no-m'>{averageStreaks.toFixed(1)}</h1> average streak
+            <h1 className='no-m'>{average.toFixed(1)}</h1> average streak
           </div>
         </div>
       </div>
     </div>
   }
+}
+
+function calc (analysis) {
+  return analysis.reduce((streak, curr) => {
+    if (curr.pomodoros.length > 0) {
+      streak.current += 1
+    } else {
+      streak.longest = streak.current > streak.longest ? streak.current : streak.longest
+      streak.streaks.push(streak.current)
+      streak.average = streak.streaks.reduce((sum, s) => sum + s, 0) / Math.max(streak.streaks.length, 1)
+      streak.current = 0
+    }
+    return streak
+  }, {longest: 0, streaks: [], current: 0, average: 0})
 }
