31dcdecc43b67bb4d1a16f5dbc5b3ad635dc6d15
Christian Fei <crifei93@gmail.com>
2019-07-13 15:00:21 +0200

    remove p-limit

--- app/src/actions/api.js
+++ app/src/actions/api.js
@@ -1,8 +1,5 @@
 import AnalyticsService from '../modules/AnalyticsService'
 import toISOSubstring from '../modules/to-iso-substring'
-import pLimit from 'p-limit'
-
-const limit = pLimit(3)
 
 export const API_CREATE_POMODORO = 'API_CREATE_POMODORO'
 export const API_CREATE_POMODORO_SUCCESS = 'API_CREATE_POMODORO_SUCCESS'
@@ -144,88 +141,6 @@ export function apiUpdateTodo (todo) {
       getState().user && AnalyticsService.track('update-todo-error', err)
       return dispatch({type: API_UPDATE_TODO_ERROR, payload: 'Something went wrong. Please try again'})
     })
-  }
-}
-
-export function apiCreatePomodoros (pomodoros) {
-  return (dispatch, getState) => {
-    const promises = pomodoros.map(pomodoro => limit(() => {
-      dispatch({type: API_CREATE_POMODORO, payload: null})
-
-      const body = JSON.stringify(pomodoro)
-      const url = window.USE_PROD
-        ? 'https://api.pomodoro.cc/pomodoros'
-        : 'http://localhost:3000/pomodoros'
-
-      return window.fetch(url, {
-        method: 'POST',
-        body,
-        cache: 'no-cache',
-        mode: 'cors',
-        credentials: 'include',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => response.json())
-      .then(data => {
-        if (data.error) {
-          getState().user && AnalyticsService.track('create-pomodoro-error', data.error)
-          return dispatch({type: API_CREATE_POMODORO_ERROR, payload: data.error})
-        }
-        getState().user && AnalyticsService.track('create-pomodoro-success', data)
-        dispatch({type: API_CREATE_POMODORO_SUCCESS, payload: data})
-        apiGetPomodorosForDay()(dispatch, getState)
-      })
-      .catch(err => {
-        getState().user && AnalyticsService.track('create-pomodoro-error', err)
-        return dispatch({type: API_CREATE_POMODORO_ERROR, payload: 'Something went wrong. Please try again'})
-      })
-    }))
-
-    Promise.all(promises)
-  }
-}
-
-export function apiCreateTodos (todos) {
-  return (dispatch, getState) => {
-    const promises = todos.map(todo => limit(() => {
-      dispatch({type: API_CREATE_TODO, payload: null})
-
-      const body = JSON.stringify(todo)
-      const url = window.USE_PROD
-        ? 'https://api.pomodoro.cc/todos'
-        : 'http://localhost:3000/todos'
-
-      return window.fetch(url, {
-        method: 'POST',
-        body,
-        cache: 'no-cache',
-        mode: 'cors',
-        credentials: 'include',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => response.json())
-      .then(data => {
-        if (data.error) {
-          getState().user && AnalyticsService.track('create-todo-error', data.error)
-          return dispatch({type: API_CREATE_TODO_ERROR, payload: data.error})
-        }
-        getState().user && AnalyticsService.track('create-todo-success', data)
-        dispatch({type: API_CREATE_TODO_SUCCESS, payload: data})
-        apiGetTodolist()(dispatch, getState)
-      })
-      .catch(err => {
-        getState().user && AnalyticsService.track('create-todo-error', err)
-        return dispatch({type: API_CREATE_TODO_ERROR, payload: 'Something went wrong. Please try again'})
-      })
-    }))
-
-    Promise.all(promises)
   }
 }
 
