53ff1c6b6e15a4c279a0f07c3b9ec5072d0ce369
Christian Fei <crifei93@gmail.com>
2020-02-06 22:21:22 +0100

    clean heatmap

--- app/src/components/Heatmap.js
+++ app/src/components/Heatmap.js
@@ -4,16 +4,42 @@ import './Heatmap.styl'
 export default class Heatmap extends Component {
   render () {
     const { analytics, onChangeDate, showCurrent = false, current } = this.props
-    if (analytics.length < 3) return null
+
+    if (analytics.length < 3) {
+      return null
+    }
+
+    const groupedByMonth = analytics.reduce((acc, curr) => {
+      const month = new Date(curr.day).getUTCMonth() + 1
+      const year = new Date(curr.day).getUTCFullYear()
+      const monthYear = `${month}`.padStart(2, '0') + '-' + year
+      if (!acc.find(a => a.monthYear === monthYear)) {
+        return acc.concat([{
+          monthYear,
+          data: [curr]
+        }])
+      }
+      return acc.map(a => {
+        if (a.monthYear !== monthYear) return a
+        return Object.assign(a, {data: a.data.concat(curr)})
+      })
+    }, [])
+
+    console.log('groupedByMonth', groupedByMonth)
+
     return <div className='heatmap-container'>
-      {onChangeDate && <strong>Click on any square below to see the daily stats</strong>}
+      {/* {onChangeDate && <strong>Click on any square below to see the daily stats</strong>} */}
       <br />
       <div className='heatmap'>
-        {(analytics || []).map(a => {
-          return <span className={`tooltip ${showCurrent && current === a.day && 'current'}`} onClick={() => onChangeDate && onChangeDate(a.day)} title={a.day} data-value={a.pomodoros.length}>
-            &nbsp;
-            {/* <span className='tooltip-content'>{a.day} {a.pomodoros.length}</span> */}
-          </span>
+        {(groupedByMonth || []).map(({monthYear, data = []} = {}) => {
+          return <div className='month'>
+            <h3>{monthYear}</h3>
+            {data.map(a => {
+              return <span className={`tooltip ${showCurrent && current === a.day && 'current'}`} onClick={() => onChangeDate && onChangeDate(a.day)} title={a.day} data-value={a.pomodoros.length}>
+                {a.day.substring(8)}
+              </span>
+            })}
+          </div>
         })}
       </div>
     </div>
