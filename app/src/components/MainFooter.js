cb1f1b623966453ee897dab661bfcfc931e15840
TheGoose <18522748+theg00s3@users.noreply.github.com>
2020-05-08 15:37:58 +1000

    Removes more references to pomodoro.cc and christian

--- app/src/components/MainFooter.js
+++ app/src/components/MainFooter.js
@@ -6,16 +6,7 @@ export default class MainFooter extends Component {
     return <footer className='main-footer-wrapper'>
       <div>
         <p>
-          Follow us on <a href='https://twitter.com/pomodoro_cc' target='_blank'>Twitter</a> for the latest news! Also give us feedback and contact us for feature requests!
-        </p>
-        <p>
-          For feedback, bug reports or pull requests, please use <a href='https://github.com/christian-fei/pomodoro.cc/' target='_blank'>Github</a> or contact us on <a href='https://twitter.com/pomodoro_cc' target='_blank'>Twitter</a>
-        </p>
-        <p>
-          Made with &lt;3 by <a href='https://christianfei.com' target='_blank'>Christian Fei</a>
-        </p>
-        <p>
-          &copy; 2014 - {new Date().toISOString().substr(0, 4)} Christian Fei
+          For feedback, bug reports or pull requests, please use <a href='https://github.com/theg00s3/pomodoro.cc/' target='_blank'>Github</a>
         </p>
         <p>
           <a href='/gdpr'>GDPR privacy policy</a> &nbsp; - &nbsp; <a href='/tos'>Terms of Service</a>
